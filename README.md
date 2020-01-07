906be46597839a6ddd1383fc8d79b829b7a5d7d7
kvellur <karthik_vellur@intuit.com>
2020-01-07 07:54:21 +0530

    create contact commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
+Collection of simple ReactJS projects which i developed for fine-graining my skills on ReactJS & Javascript as part of Udacity's React Nano degree program
+
+Each branch is a different project
+
+The main project is on the branch 'contacts' which shows a list of contacts, with the ability of search, add and remove contacts
+
 ## Project Setup
 
-* clone the Project
+* clone the Project and the desired branch
 * install the dependencies - `npm install`
+* Start the webpack server - `npm start`
+
+## Backend server
+
+* 
--- src/App.js
+++ src/App.js
@@ -30,6 +30,15 @@ class App extends Component {
         ContactsAPI.remove(contact)
     }
 
+  createContact = (contact) => (
+        ContactsAPI.create(contact)
+            .then((contact) => {
+                this.setState((currentState) => ({
+                    contacts: currentState.contacts.concat([contact])
+                }))
+            })
+  )
+
   render() {
     return (
       <div>
@@ -43,7 +52,18 @@ class App extends Component {
                   )
               }
           />
-          <Route path='/create' component={CreateContact}/>
+          <Route path='/create' render=
+              { ({history}) => (
+                        <CreateContact
+                            onCreateContact={ (contact) => {
+                                    this.createContact(contact)
+                                    history.push('/')
+                                }
+                            }
+                        />
+                      )
+              }
+          />
       </div>
     );
   }
